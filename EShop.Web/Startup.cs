d753629f2b731a59a6607598ff4996f9c3868bc6
Kamol Roy <kamol.roy9943@gmail.com>
2019-03-31 21:53:37 +0600

    Add project files.

---
+++ EShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EShop.Web.Startup))]
+namespace EShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
